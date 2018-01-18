04389b98aa45cffaf90f95ef42fab6b027ca0d48
Shivani Daftardar <shivani_daf@yahoo.com>
2018-01-18 10:22:18 -0500

    first commit

---
+++ src/test/java/org/com/easybix/restassuredframework/SampleTestClass1.java
@@ -0,0 +1,15 @@
+package org.com.easybix.restassuredframework;
+
+import org.testng.annotations.Test;
+
+public class SampleTestClass1 {
+	@Test
+	public void firstTest() {
+		System.out.println("My First Tests");
+	}
+	@Test
+	public void secondTest() {
+		System.out.println("My Second Tests");
+	}
+
+}
---
+++ src/test/java/org/com/easybix/restassuredframework/SampleTestClass2.java
@@ -0,0 +1,16 @@
+package org.com.easybix.restassuredframework;
+
+import org.testng.annotations.Test;
+
+public class SampleTestClass2 {
+
+	@Test
+	public void thridTest() {
+		System.out.println("My Third Tests");
+	}
+	@Test
+	public void fourthTest() {
+		System.out.println("My Fourth Tests");
+	}
+	
+}
